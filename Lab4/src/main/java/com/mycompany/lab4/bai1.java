78f36e83584156e27e8bbe0caa6906e3a4684fbd
Kuro <Kuro@172.16.90.197>
2025-09-27 08:40:43 +0700

    Nộp bài Lab 4

--- Lab4/src/main/java/com/mycompany/lab4/bai1.java
+++ Lab4/src/main/java/com/mycompany/lab4/bai1.java
@@ -11,6 +11,6 @@ package com.mycompany.lab4;
 public class bai1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("toi ten la pham minh hoang");
     }
 }
